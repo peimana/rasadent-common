44e6a250cfd4c293b555b6f063407dd8b1cb9572
Amir Vahedi <amirvahedix@gmail.com>
2025-09-20 13:59:36 +0330

    Added enums for promotion module

---
+++ src/Promotion/Enums/PromotionLocationEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rasadent\Common\Promotion\Enums;
+
+enum PromotionLocationEnum: string
+{
+    case Top = "top";
+    case Middle = "middle";
+    case Middle2 = "middle_2";
+    case Bottom = "bottom";
+    case Full = "full";
+    case Navbar = "navbar";
+}
---
+++ src/Promotion/Enums/PromotionTypeEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Rasadent\Common\Promotion\Enums;
+
+enum PromotionTypeEnum: string
+{
+    case Banner = "banner";
+    case Slider = "slider";
+}
