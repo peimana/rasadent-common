b434b03dff112bb8cbccabaf951c70083fb32322
Amir Vahedi <amirvahedix@gmail.com>
2025-12-21 14:37:45 +0330

    update

--- src/Catalog/Enums/CatalogFileStatusEnum.php
+++ src/Catalog/Enums/CatalogFileStatusEnum.php
@@ -5,6 +5,7 @@ namespace Rasadent\Common\Catalog\Enums;
 enum CatalogFileStatusEnum: string
 {
     case WAITING = 'waiting';
+    case IN_PROGRESS = 'in_progress';
     case PROCESSED = 'processed';
     case INVALID = 'invalid';
 }
