46b5eb24e1f1cb9811228ea5ed91a5a30117746c
Amir Vahedi <amirvahedix@gmail.com>
2026-01-08 12:02:24 +0330

    Implemented AppEnum

---
+++ src/Shared/Enums/AppEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Rasadent\Common\Shared\Enums;
+
+enum AppEnum: string
+{
+    case MARKETPLACE = 'marketplace';
+    case CHORTKEH = 'chortkeh';
+    case ADS = 'ads';
+    case VENDOR = 'vendor';
+    case APPLICATION = 'application';
+}
